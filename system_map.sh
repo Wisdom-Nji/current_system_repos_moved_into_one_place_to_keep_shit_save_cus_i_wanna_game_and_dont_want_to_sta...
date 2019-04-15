1571e19b199085f768286b6d9870cf044bd3ff69
Sherlock Wisdom <wisdomnji@gmail.com>
2019-04-15 17:51:26 +0100

    Mon, 15 Apr 2019 17:51:26 +0100

--- system_map.sh
+++ system_map.sh
@@ -2,3 +2,13 @@
 
 
 mkdir -p $HOME/Desktop && git clone git@github.com:Wisdom-Nji/stuff_you_keep_around_when_you_think_your_laptop_might_be_stolen_or_destroyed_and_you_dont_want_to...git
+mkdir -p $HOME/Desktop/Clients && git clone git@bitbucket.org:dev_afkanerds/mhealth-app.git
+mkdir -p $HOME/Desktop/OSS/Afkanerd && git clone git@github.com:Wisdom-Nji/Linux-Tools.git
+mkdir -p $HOME/Desktop/OSS/Afkanerd && git clone git@github.com:Afkanerd/mmcl1_m0d3m.git
+mkdir -p $HOME/Desktop/OSS/Afkanerd && git clone git@github.com:Afkanerd/OpenSmsOs.git
+mkdir -p $HOME/Desktop/OSS/Afkanerd && git clone git@github.com:Afkanerd/m0d3m_u55d.git
+mkdir -p $HOME/Desktop/OSS/Afkanerd && git clone git@github.com:Afkanerd/mmcl1_5m5.git
+mkdir -p $HOME/Desktop/Projects/Afkanerd && git clone git@github.com:Wisdom-Nji/Linux_SMS_server.git
+mkdir -p $HOME/Desktop/Projects/Afkanerd && git clone git@bitbucket.org:MCBAfkanerd/website.git
+sudo mkdir -p /var/www/tbapp && git clone git@bitbucket.org:MCBAfkanerd/tbapp.git
+sudo mkdir -p /var/www && git clone git@bitbucket.org:MCBAfkanerd/tbapponlinedashboard.git
