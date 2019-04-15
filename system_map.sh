7d512fd3e5e68ec42bde9b1f740e348bd3e894e9
Sherlock Wisdom <wisdomnji@gmail.com>
2019-04-15 14:17:39 +0100

    Mon, 15 Apr 2019 14:17:39 +0100

--- system_map.sh
+++ system_map.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 
+mkdir -p $HOME/Desktop && git clone git@github.com:Wisdom-Nji/stuff_you_keep_around_when_you_think_your_laptop_might_be_stolen_or_destroyed_and_you_dont_want_to...git
